c134d38e74d349513f728314240cf057c588df2f
anseki <AnSeki.aff@gmail.com>
2013-11-01 21:57:39 +0900

    first commit

---
+++ jquery.ui.tooltip.altposition.js
@@ -0,0 +1,26 @@
+
+(function($, undefined) {
+
+$.widget('ui.tooltip', $.ui.tooltip, {
+  _open: function() {
+    var jq = this.element,
+      altPosition = this.options.altPosition,
+      orgPos, res;
+    if (altPosition && (
+        altPosition.tagName && jq.get(0).nodeName.toLowerCase() === altPosition.tagName ||
+        altPosition.minOuterWidth && jq.outerWidth() >= altPosition.minOuterWidth ||
+        altPosition.maxOuterWidth && jq.outerWidth() <= altPosition.maxOuterWidth ||
+        altPosition.minOuterHeight && jq.outerHeight() >= altPosition.minOuterHeight ||
+        altPosition.maxOuterHeight && jq.outerHeight() <= altPosition.maxOuterHeight ||
+        altPosition.callback && altPosition.callback.call(this)
+        )) {
+      orgPos = this.options.position; // Save
+      this.options.position = altPosition.position;
+    }
+    res = this._superApply(arguments);
+    if (orgPos) { this.options.position = orgPos; } // Restore
+    return res;
+  }
+});
+
+}(jQuery));
